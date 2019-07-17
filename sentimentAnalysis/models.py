23e73f097fe0174faaa3a8eeb16d4c2b2107706d
victoriagjh <akrso06197@khu.ac.kr>
2019-07-17 15:40:29 -0700

    store&display the tools
    - store the tools that user checked
    - display the tools in result page

--- sentimentAnalysis/models.py
+++ sentimentAnalysis/models.py
@@ -5,10 +5,3 @@ from django.core.validators import FileExtensionValidator
 class UploadFileModel(models.Model):
     title = models.TextField(default='')
     file = models.FileField()
-
-class ToolModel(models.Model):
-    tool1 = models.BooleanField(default=False)
-    tool2 = models.BooleanField(default=False)
-    tool3 = models.BooleanField(default=False)
-    tool4 = models.BooleanField(default=False)
-    tool5 = models.BooleanField(default=False)
--- sentimentAnalysis/views.py
+++ sentimentAnalysis/views.py
@@ -1,27 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import UploadFileForm
-from .forms import ToolsForm
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 def sentimentAnalysis(request):
-    toolForm = ToolsForm()
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            form.name = request.FILES['file'].name
-            content = handle_uploaded_file(request.FILES['file'])
-            form.content = content
-            context = {
-                'form':form,
-            }
-            return render(request, "result_page.html",context)
+            tools=[]
+            if type(request.POST.get('nltk')) !=type(None):
+                tools.append(request.POST.get('nltk'))
+            if type(request.POST.get('spacy')) != type(None):
+                tools.append(request.POST.get('spacy'))
+            if type(request.POST.get('scikit')) != type(None):
+                tools.append(request.POST.get('scikit'))
+            if type(request.POST.get('r')) != type(None):
+                tools.append(request.POST.get('r'))
+            if type(request.POST.get('bagOfWords')) != type(None):
+                tools.append(request.POST.get('bagOfWords'))
+            if tools:
+                form.save()
+                form.name = request.FILES['file'].name
+                content = handle_uploaded_file(request.FILES['file'])
+                form.content = content
+                form.tool = tools
+                context = {
+                    'form':form,
+                    }
+                return render(request, "result_page.html",context)
     else:
         form = UploadFileForm()
     context = {
         'form':form,
-        'toolForm':toolForm
     }
     return render(request, 'main_page.html', context)
 
