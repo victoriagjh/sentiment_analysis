3b81f68e40d15538a886737d847b01de8c3a6cff
victoriagjh <akrso06197@khu.ac.kr>
2019-09-17 23:08:42 -0700

    update settings.py

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sentimentAnalysis',
     'crispy_forms',
+    'whitenoise.runserver_nostatic',
+
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
