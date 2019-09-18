634bc9a2c3d3e62b3e1a1775d3ac3a2646d369d1
victoriagjh <akrso06197@khu.ac.kr>
2019-09-17 21:14:24 -0700

    commit for heroku

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,6 +148,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 from django.contrib.messages import constants as message_constants
 MESSAGE_LEVEL = message_constants.DEBUG
+
+import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
