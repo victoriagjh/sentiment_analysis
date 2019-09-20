add077fd583fa1ebf1e269a87e2135be00b843a0
victoriagjh <akrso06197@khu.ac.kr>
2019-09-20 15:57:51 -0700

    Update wsgi.py

--- sentiment_analysis/wsgi.py
+++ sentiment_analysis/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-#import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sentiment_analysis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sentiment_analysis.settings')
 
 application = get_wsgi_application()
